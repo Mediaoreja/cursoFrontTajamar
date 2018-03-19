cc8c5ef356ae73346bb435708118d3ab4aeb2750
Mediaoreja <mediaoreja@gmail.com>
2018-03-19 13:04:47 +0100

    ejercicio sumar pares resuelto

--- unidad_JS/03_sentencias/js/sumarpares.js
+++ unidad_JS/03_sentencias/js/sumarpares.js
@@ -3,12 +3,12 @@ let aDatos2 = [1, 2, 3, 4, 5, 6]
 
 function sumarPares(x) {
     let suma = 0
-    for (let i = 0; i < x.length; i++) {
+    for (let i = 0; i < x.length; i++) {        
         if ( isNaN(x[i]/2) ? -1 : x[i]%2 == 0 ) { 
             suma += x[i]
-        }      
+        }           
     }
-        console.log(suma)  
+    console.log(suma)  
 }
 
 function sumarImpares(x) {
